4f1bfeea8bbd436b35507f01cfc51337f2ea89b4
Tejas Iyer <tejas.iyer@outlook.com>
2020-07-10 11:19:50 -0400

    package com.hack3;
    public class Main {
    
        public static void main(String[] args) {
    
            Scanner in = new Scanner(System.in);
            System.out.println("Print your message boi: ");
        }
    }

--- src/com/hack3/Main.java
+++ src/com/hack3/Main.java
@@ -3,6 +3,8 @@ package com.hack3;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Print your message boi: ");
     }
 }
