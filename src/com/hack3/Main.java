e2b7f2a03224ffdf33988287ab34c763eb2d45b4
Tejas Iyer <tejas.iyer@outlook.com>
2020-07-10 11:29:07 -0400

    bruh momment v4

--- src/com/hack3/Main.java
+++ src/com/hack3/Main.java
@@ -1,8 +1,14 @@
 package com.hack3;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Print your message boi: ");
+        String s = in.nextLine();
+        System.out.println(s);
     }
 }
